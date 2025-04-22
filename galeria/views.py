c0d09301db5f0054af0939e6ee922f25aadf85cb
Matheus Peralta <61744515+perseusbr25@users.noreply.github.com>
2025-04-22 13:39:01 -0300

    Iniciando o banco de dados utilizando SQLite e DjangoORM

--- galeria/views.py
+++ galeria/views.py
@@ -2,7 +2,22 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'galeria/index.html')
+
+    dados={
+        1:{"nome": "Nebulosa Carina",
+           "legenda":"webtelescope.org/ NASA / James Webb"},
+        2:{"nome": "Galáxia Centauri",
+           "legenda":"webtelescope.org/ NASA / James Webb"},
+        3:{"nome": "Planeta Mustafar",
+           "legenda":"webtelescope.org/ NASA / James Webb"},
+        4:{"nome": "Planeta Naboo",
+           "legenda":"webtelescope.org/ NASA / James Webb"},
+        5:{"nome": "Planeta Mandalore",
+           "legenda":"webtelescope.org/ NASA / James Webb"},
+         6:{"nome": "Estrela Cardinali",
+           "legenda":"webtelescope.org/ NASA / James Webb"}
+    }
+    return render(request, 'galeria/index.html', {"cards":dados})
 
 def imagem(request):
     return render(request, 'galeria/imagem.html')
