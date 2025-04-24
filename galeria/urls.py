9408f1d06a86719587faefc7f8c7011f68dc7078
Matheus Peralta <61744515+perseusbr25@users.noreply.github.com>
2025-04-24 17:20:32 -0300

    construindo formulário login e cadastro

--- galeria/urls.py
+++ galeria/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
 from galeria.views import index, imagem, buscar
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('',index, name='index'),
     path('imagem/<int:foto_id>', imagem, name='imagem'),
     path("buscar", buscar, name="buscar"),
-] + static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
+] 
